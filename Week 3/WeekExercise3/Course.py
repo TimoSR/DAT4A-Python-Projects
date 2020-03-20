f7309534aa6513b964790863bf9a5676a89fb9f4
Unknown <s144146@student.dtu.dk>
2020-03-20 12:55:48 +0100

    update

--- Week 3/WeekExercise3/Course.py
+++ Week 3/WeekExercise3/Course.py
@@ -2,8 +2,8 @@ class Course:
 
     # 3. Each course has name, classroom, teacher, ETCS and optional grade if course is taken.
 
-    def __init__(self, name, classroom, teacher, ECTS, grade=0):
-        self.name = name
+    def __init__(self, course_name, classroom, teacher, ECTS, grade=0):
+        self.name = course_name
         self.classroom = classroom
         self.teacher = teacher
         self.ECTS = ECTS
