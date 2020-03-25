ccfd375ce47512a4525f3925fdfc6682777cfaef
Unknown <s144146@student.dtu.dk>
2020-03-25 14:12:27 +0100

    Update

--- Week 6/WeekExercise6/Exercise1/Modules/URL_Downloader.py
+++ Week 6/WeekExercise6/Exercise1/Modules/URL_Downloader.py
@@ -1,3 +1,5 @@
+import wget
+
 class URL_Downloader():
 
     def __init__(self, url_list):
@@ -6,3 +8,13 @@ class URL_Downloader():
 
     def __iter__(self):
         return self
+
+    def multi_download(self):
+
+    def __next__(self):
+
+    def url_list_genrator(self):
+
+    def avg_amount_vowels(self, text):
+
+    def hardest_read(self   ):
