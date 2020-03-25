55167e4afd5e0a58ed600fbf2bfb5291e576ef1f
Unknown <s144146@student.dtu.dk>
2020-03-25 14:24:49 +0100

    Update

--- Week 6/WeekExercise6/Exercise1/Modules/URL_Downloader.py
+++ Week 6/WeekExercise6/Exercise1/Modules/URL_Downloader.py
@@ -1,20 +1,52 @@
 import wget
 
+#Create a module containing a class with the following methods:
+# 1. init(self, url_list)
+# 2. download(url,filename) raises NotFoundException when url returns 404
+# 3. multi_download() uses threads to download multiple urls as text and stores filenames as a property
+# 4. iter() returns an iterator
+# 5. next() returns the next filename (and stops when there are no more)
+# 6. urllist_generator() returns a generator to loop through the urls
+# 7. avg_vowels(text) - a rough estimate on readability returns average number of vowels in the words of the text
+# 8. hardest_read() returns the filename of the text with the highest vowel score (use all the cpu cores on the computer for this work.
+
+class NotFoundException(ValueError):
+    def __init__(self, *args, **kwargs):
+        ValueError.__init__(self, *args, *kwargs)
+
 class URL_Downloader():
 
+    # 1.
+
     def __init__(self, url_list):
+        self.url_list = url_list
+
+    # 2.
 
     def download(self, url, filename):
+        return wget.download(url, filename)
+
+    # 3.
+
+    def multi_download(self):
+
+    # 4.
 
     def __iter__(self):
         return self
 
-    def multi_download(self):
+    # 5.
 
     def __next__(self):
 
+    # 6.
+
     def url_list_genrator(self):
 
+    # 7.
+
     def avg_amount_vowels(self, text):
+
+    # 8.
 
     def hardest_read(self   ):
